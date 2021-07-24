7b403fad4a46a2a735aa025cbed3c10b6a7102af
Eightaan <57832730+Eightaan@users.noreply.github.com>
2021-07-24 23:06:18 +0200

    Delete tagteamcancer.lua

--- lua/tagteamcancer.lua
+++
@@ -1,7 +0,0 @@
-local BLM_old_tag_team_tagged_function = PlayerAction.TagTeamTagged.Function
-
-PlayerAction.TagTeamTagged.Function = function (...)
-	if not Berserker_Help_mod._current.tag_team_cancer then
-		BLM_old_tag_team_tagged_function(...)
-	end
-end
